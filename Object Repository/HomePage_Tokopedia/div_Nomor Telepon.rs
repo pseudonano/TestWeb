d2a83853a77cecabc60b1afbe68fd21ce05545f9
pseudonano <teguhpambudi1251@gmail.com>
2025-01-13 00:27:53 +0700

    add object repository topup menu

---
+++ Object Repository/HomePage_Tokopedia/div_Nomor Telepon.rs
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Nomor Telepon</name>
+   <tag></tag>
+   <elementGuidId>845a8189-6535-4e41-b19e-10ff83cb8478</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.css-ufaufa.erj7th73</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='clientNumber']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>91066a81-74e2-4680-b080-f46a614c41c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>css-ufaufa erj7th73</value>
+      <webElementGuid>e5a1520a-83ad-4340-89bf-bec5eaf4a22c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;zeus-root&quot;)/div[@class=&quot;css-8atqhb&quot;]/main[@class=&quot;css-15x0u31&quot;]/section[@class=&quot;css-stopwq bordered&quot;]/div[@class=&quot;css-3kh9t4&quot;]/div[@class=&quot;css-1ukamyt m24&quot;]/div[@class=&quot;css-1cysb4j even left&quot;]/section[@class=&quot;css-7lrhbb-unf-card eeeacht0&quot;]/section[@class=&quot;css-1gw7mzg&quot;]/div[@class=&quot;css-iijm2c&quot;]/div[@class=&quot;css-he6f0n&quot;]/div[@class=&quot;css-1f5gle0&quot;]/div[@class=&quot;css-10t5fvw&quot;]/div[@class=&quot;css-1fms4as erj7th70&quot;]/div[@class=&quot;css-79elbk erj7th72&quot;]/div[@class=&quot;css-ufaufa erj7th73&quot;]</value>
+      <webElementGuid>01107e1e-5ccc-460c-b63a-581fd79c1b95</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='zeus-root']/div/main/section/div/div/div[2]/section/section/div/div[2]/div/div/div/div/div</value>
+      <webElementGuid>175724b2-21ee-4282-a6a8-876e5d06ee00</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nomor Telepon'])[1]/following::div[3]</value>
+      <webElementGuid>217f73b3-2bd3-4223-ae8f-4d10793db54f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Lihat Semua'])[1]/following::div[13]</value>
+      <webElementGuid>e7c78afa-dbc7-41d0-bc71-8607dd00d2bd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nominal'])[1]/preceding::div[2]</value>
+      <webElementGuid>4434b388-0dd8-466b-ba4d-5dcc5a2ad15b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Beli'])[1]/preceding::div[5]</value>
+      <webElementGuid>f89ff345-4780-4a74-b537-577db024c08a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div/div[2]/div/div/div/div/div</value>
+      <webElementGuid>a8407e27-93b0-405a-9634-d2257dea1e27</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
