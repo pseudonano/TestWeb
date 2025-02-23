7bb86e035566b118fb9b4e88e35ed38b553e34aa
pseudonano <teguhpambudi1251@gmail.com>
2025-02-23 18:29:21 +0700

    refactor test cases folder structure

--- Scripts/OrangeHRM/common/TC_logout/Script1740308330082.groovy
+++ Scripts/OrangeHRM/common/TC_logout/Script1740308330082.groovy
@@ -17,21 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/span_headerProfile'))
 
-WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-
-WebUI.setText(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/input_Username_username'), 'Admin')
-
-WebUI.setEncryptedText(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
-
-WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/button_Login'))
-
-WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/a_Recruitment'))
-
-//WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/span_Thanh Vo'))
-
-//WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/a_Logout'))
-
-//WebUI.closeBrowser()
-
+WebUI.click(findTestObject('Object Repository/OrangeHRM/Page_OrangeHRM/a_Logout'))
